d88b22eca70937059c7c263cc9bc72218d39b832
root <mw.mzperx@gmail.com>
2013-02-28 05:58:20 -0600

    Added time measuring to test.

--- src/test_gy81.cpp
+++ src/test_gy81.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include "gy-81.h"
-
+#include <sys/time.h>
+#include <cstdio>
 
 using namespace std;
 
+typedef unsigned long long timestamp_t;
+
+static timestamp_t get_timestamp ()
+{
+  struct timeval now;
+  gettimeofday (&now, NULL);
+  return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
+}
+
+
 int main(int argc, char* argv[])
 {
-    GY_81 imu;
-    while(true)
-    {
-        imu.print();
-        imu.update();	
-        sleep(1);
-    }
-    return 0;
+  GY_81 imu;
+  while(true)
+  {
+    timestamp_t t0 = get_timestamp();
+
+    imu.update();	
+    imu.print();
+
+    timestamp_t t1 = get_timestamp();
+    std::cout << (t1 - t0) / 1000000.0L << std::endl; // time to take sample
+  }
+  return 0;
 }
